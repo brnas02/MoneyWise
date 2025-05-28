98bf4d2e4bc072e1283959a3378fd803965ebccf
brnas02 <aluno23025@ipt.pt>
2025-05-28 10:55:03 +0100

    Transacoes TableDB
    Modificação do nome da tabela Transacoes

--- Entities/AppDbContext.cs
+++ Entities/AppDbContext.cs
@@ -1,17 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-namespace DWebProjetoFinal.Entities {
-    public class AppDbContext : DbContext {
+namespace DWebProjetoFinal.Entities
+{
+    public class AppDbContext : DbContext
+    {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<UserAccount> UserAccounts { get; set; }
-
-<<<<<<< Updated upstream
-=======
         public DbSet<Transacao> Transacoes { get; set; }
 
->>>>>>> Stashed changes
-        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
             modelBuilder.Entity<UserAccount>().Property(u => u.Type).IsRequired();
             base.OnModelCreating(modelBuilder);
         }
