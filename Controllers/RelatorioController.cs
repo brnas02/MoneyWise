07b8c4747c0322234061b79580ac6b461402ebf3
goncalo-m-ferreira <103708923+goncalo-m-ferreira@users.noreply.github.com>
2025-06-22 01:47:47 +0100

    Adição das views do Relatório
    Adição das views em relação aos Relatórios

---
+++ Controllers/RelatorioController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DWebProjetoFinal.Controllers
+{
+    [Authorize(Roles = "Empresarial,Admin")]
+    public class RelatorioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Mensal()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Mensal(int mes, int ano)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Anual()
+        {
+            return View();
+        }
+    }
+}
